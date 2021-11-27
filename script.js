bbd7386c5e8638e7462ca62e49dd22f91ea2947f
Torfa <alextison@icloud.com>
2021-11-27 18:26:39 +0100

    Landing About Section Full Release

---
+++ script.js
@@ -0,0 +1,9 @@
+function landingButtonsIn(button){
+    buttonPath = button.querySelector("path");
+    buttonPath.style.fill = "whitesmoke";
+}
+
+function landingButtonsOut(button){
+    buttonPath = button.querySelector("path");
+    buttonPath.style.fill = "#1b1b1b";
+}
