8f32731299739c944f16db2a1d9cf68c28efdef8
Torfa <Alexandre.Tison@etu.univ-littoral.fr>
2022-02-14 13:54:11 +0100

    Mac Merge

---
+++ README.md
@@ -0,0 +1,7 @@
+V4 of my Portfolio
+
+Stacks: HTML - SCSS - JS
+
+UI Kit is available as UIKit.zip
+
+V1, V2 & V3 will be added to my GitHub before the full release of this version
---
+++ script.js
@@ -0,0 +1,9 @@
+function changeIconColorIn(button, color){
+    buttonPath = button.querySelector("path");
+    buttonPath.style.fill = color;
+}
+
+function changeIconColorOut(button, color){
+    buttonPath = button.querySelector("path");
+    buttonPath.style.fill = color;
+}
